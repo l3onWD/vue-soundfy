bff241cbc7c0bea266f7899e1ca9e58b8aa7b38b
l3on <l3onwebdev@gmail.com>
2023-10-15 16:25:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <h1>
+        Hello World
+    </h1>
+</template>
+
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Vue
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+// Init Vue
+const app = createApp(App);
+app.mount('#app');
