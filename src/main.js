14851ef2fcda0cd2d4a5f16c96c97be8b8d4956f
l3on <l3onwebdev@gmail.com>
2023-10-15 18:54:56 +0200

    added font awesome library

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faHouse, faSearch, faBook } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHouse, faSearch, faBook);
+
+
 // Init Vue
 const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.mount('#app');
