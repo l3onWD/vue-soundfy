e8447beb29409ae479f631108de4aa697cc75fc2
l3on <l3onwebdev@gmail.com>
2023-10-16 17:16:28 +0200

    added base button component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        label: String,
+        icon: String,
+        iconStyle: {
+            type: String,
+            default: 'fas'
+        },
+        size: String
+    }
+
+}
+</script>
+
+
+<template>
+    <button>
+        <FontAwesomeIcon v-if="icon" :icon="[iconStyle, icon]" :size="size" />
+        {{ label }}
+    </button>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../../assets/scss/vars' as *;
+
+button {
+    padding: 0;
+    width: 2rem;
+    height: 2rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+
+    &:hover {
+        color: $col-orange;
+    }
+}
+</style>
