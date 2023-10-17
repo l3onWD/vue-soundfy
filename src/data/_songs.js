7d584104f67c826f49831d8bf24e699c7e1bed6e
l3on <l3onwebdev@gmail.com>
2023-10-17 19:31:42 +0200

    added more test songs

--- src/data/_songs.js
+++ src/data/_songs.js
@@ -1,8 +1,8 @@
 export default [
     {
         id: 1,
-        title: 'Brano 1',
-        author: 'Unknow',
+        title: 'Might & Magic',
+        author: 'nakaradaalexander',
         duration: 357,
         albumCover: 'https://picsum.photos/id/10/300/300',
         src: './src/assets/music/test-song-01.mp3',
@@ -10,20 +10,20 @@ export default [
     },
     {
         id: 2,
-        title: 'Brano 2',
-        author: 'Unknow 2',
-        duration: 300,
+        title: 'Metal Gear',
+        author: 'Muzaproduction',
+        duration: 100,
         albumCover: 'https://picsum.photos/id/25/300/300',
-        src: '',
+        src: './src/assets/music/metal-gear-136567.mp3',
         isFavorite: false
     },
     {
         id: 3,
         title: 'Brano 3',
-        author: 'Unknow 3',
-        duration: 300,
+        author: 'nakaradaalexander',
+        duration: 213,
         albumCover: 'https://picsum.photos/id/212/300/300',
-        src: '',
+        src: './src/assets/music/woods-of-imagination-139004.mp3',
         isFavorite: false
     }
 ];
