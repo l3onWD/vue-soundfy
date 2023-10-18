fb9de101ef2a057086060cf0a37db6c2e5bf4fc2
l3on <l3onwebdev@gmail.com>
2023-10-18 21:18:12 +0200

    added more songs

--- src/data/_songs.js
+++ src/data/_songs.js
@@ -13,7 +13,7 @@ export default [
         title: 'Metal Gear',
         author: 'Muzaproduction',
         duration: 100,
-        albumCover: 'https://picsum.photos/id/25/300/300',
+        albumCover: 'https://picsum.photos/id/137/300/300',
         src: './src/assets/music/metal-gear-136567.mp3',
         isFavorite: false
     },
@@ -22,8 +22,35 @@ export default [
         title: 'Woods of Imagination',
         author: 'nakaradaalexander',
         duration: 213,
-        albumCover: 'https://picsum.photos/id/212/300/300',
+        albumCover: 'https://picsum.photos/id/83/300/300',
         src: './src/assets/music/woods-of-imagination-139004.mp3',
+        isFavorite: false
+    },
+    {
+        id: 4,
+        title: 'Lord of the Riffs',
+        author: 'nakaradaalexander',
+        duration: 387,
+        albumCover: 'https://picsum.photos/id/209/300/300',
+        src: './src/assets/music/lord-of-the-riffs-138993.mp3',
+        isFavorite: false
+    },
+    {
+        id: 5,
+        title: 'The Wizard',
+        author: 'nakaradaalexander',
+        duration: 247,
+        albumCover: 'https://picsum.photos/id/343/300/300',
+        src: './src/assets/music/the-wizard-139003.mp3',
+        isFavorite: false
+    },
+    {
+        id: 6,
+        title: 'Jökull (Metal Version)',
+        author: 'nakaradaalexander',
+        duration: 245,
+        albumCover: 'https://picsum.photos/id/443/300/300',
+        src: './src/assets/music/jokull-metal-version-138992.mp3',
         isFavorite: false
     }
 ];
