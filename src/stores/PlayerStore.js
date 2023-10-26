43be1544faaa58ba5cdd35df4e4d0681d2569503
l3on <l3onwebdev@gmail.com>
2023-10-26 20:48:05 +0200

    added player store with track fetching

---
+++ src/stores/PlayerStore.js
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+
+export const usePlayerStore = defineStore('player', {
+
+    state: () => ({
+        audioCtx: null,
+        audioGain: null,
+        audioSource: null,
+
+        isLoading: false,
+        isPlaying: false,
+        volume: 1,
+        volumePrev: 1,
+        muted: false
+    }),
+
+    getters: {
+        // currentTrack: (state) => state.nextUpList[state.nextUpIndex],
+    },
+
+    actions: {
+        fetchTrack(id) {
+
+            console.log('FETCHING');
+
+            this.isLoading = true;
+
+            axios.get(`http://127.0.0.1:8000/api/tracks/${id}/stream`, { responseType: 'arraybuffer' })
+                .then(({ data }) => {
+
+                    console.log('BUFFERING');
+
+                    this.audioCtx = new AudioContext();
+                    this.audioGain = this.audioCtx.createGain();
+                    this.audioGain.gain.value = 1;
+                    this.volumeValuePrev = 1;
+                    this.audioGain.connect(this.audioCtx.destination);
+
+                    this.audioCtx.decodeAudioData(data,
+                        buffer => {
+                            this.audioSource = this.audioCtx.createBufferSource();
+                            this.audioSource.buffer = buffer;
+                            this.audioSource.connect(this.audioGain);
+                            this.audioSource.start(0);
+
+                            this.isLoading = false;
+                            console.log('DONE');
+
+                        }),
+                        err => {
+                            console.log(err);
+                        };
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    },
+});
