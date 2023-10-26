61fa96eb82f41eb40258294cb8ddfbf579ea38a9
l3on <l3onwebdev@gmail.com>
2023-10-26 20:57:22 +0200

    centralized init and stop

--- src/stores/PlayerStore.js
+++ src/stores/PlayerStore.js
@@ -27,19 +27,18 @@ export const usePlayerStore = defineStore('player', {
 
             this.isLoading = true;
 
+            if (this.audioCtx) this.stopTrack();
+
             axios.get(`http://127.0.0.1:8000/api/tracks/${id}/stream`, { responseType: 'arraybuffer' })
                 .then(({ data }) => {
 
                     console.log('BUFFERING');
 
-                    this.audioCtx = new AudioContext();
-                    this.audioGain = this.audioCtx.createGain();
-                    this.audioGain.gain.value = 1;
-                    this.volumeValuePrev = 1;
-                    this.audioGain.connect(this.audioCtx.destination);
+                    this.initAudio();
 
                     this.audioCtx.decodeAudioData(data,
                         buffer => {
+
                             this.audioSource = this.audioCtx.createBufferSource();
                             this.audioSource.buffer = buffer;
                             this.audioSource.connect(this.audioGain);
@@ -56,6 +55,22 @@ export const usePlayerStore = defineStore('player', {
                 .catch(err => {
                     console.log(err);
                 })
+        },
+
+        initAudio() {
+            this.audioCtx = new AudioContext();
+            this.audioGain = this.audioCtx.createGain();
+            this.audioGain.gain.value = 1;
+            this.volumeValuePrev = 1;
+            this.audioGain.connect(this.audioCtx.destination);
+        },
+
+        stopTrack() {
+            this.audioSource.disconnect();
+            this.audioGain.disconnect()
+            this.audioSource = null;
+            this.audioGain = null;
+            this.audioCtx.close();
         }
     },
 });
