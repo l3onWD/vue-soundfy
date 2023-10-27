4bc833cf424660116de6af82f53eb42ddee31604
l3on <l3onwebdev@gmail.com>
2023-10-27 19:22:43 +0200

    added empty list check

--- src/components/media/MediaSection.vue
+++ src/components/media/MediaSection.vue
@@ -29,11 +29,13 @@ export default {
         <h2 class="mb-4">{{ title }}</h2>
 
         <!-- Carousel -->
-        <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4">
+        <div v-if="mediaList.length" class="row row-cols-2 row-cols-sm-3 row-cols-md-4">
             <div v-for="media in mediaList" :key="media.id" class="col">
                 <MediaCard :media="media" />
             </div>
         </div>
+
+        <span v-else>Nothing founded.</span>
 
     </section>
 </template>
