ddadfcc4a3a435e2726fa2d99374830ca73eff2f
l3on <l3onwebdev@gmail.com>
2023-10-27 19:17:09 +0200

    added app loader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+
+    props: {
+        isVisible: Boolean
+    }
+
+}
+</script>
+
+
+<template>
+    <div v-if="isVisible" class="app-loader">
+        <FontAwesomeIcon icon="fas fa-spinner" spin-pulse size="5x" />
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.app-loader {
+    position: absolute;
+    inset: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    background-color: rgba($color: #000, $alpha: 0.8);
+    z-index: 1;
+}
+</style>
