f5bba14a55aafd032e5d67d5c5ead71a620d042f
l3on <l3onwebdev@gmail.com>
2023-10-31 19:59:28 +0100

    refactored router name

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Router
-import { router } from './router';
+import { router } from './router/';
 
 // Pinia
 import { createPinia } from 'pinia'
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
+import PlaylistDetailPage from '@/pages/PlaylistDetailPage.vue';
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: HomePage },
+        { path: '/playlists/:id', name: 'playlist-detail', component: PlaylistDetailPage },
 
         { path: '/:pathMatch(.*)*', redirect: '/' },
     ]
