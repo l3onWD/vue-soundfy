2b77c85cdff84eb304bf6f4358e8eb49ea282f7d
l3on <l3onwebdev@gmail.com>
2023-11-01 17:59:16 +0100

    added base style

--- src/pages/PlaylistDetailPage.vue
+++ src/pages/PlaylistDetailPage.vue
@@ -21,6 +21,22 @@ export default {
         isLoading: 0
     }),
 
+    computed: {
+        totlaDuration() {
+
+            // Get total duration
+            const totlSecs = this.media.tracks.reduce((tot, { duration }) => tot += duration, 0);
+
+            // Format time
+            const hours = Math.floor(totlSecs / 3600);
+            const minutes = Math.floor(totlSecs / 60);
+            const seconds = Math.floor(totlSecs % 60);
+
+            // Return time string
+            return hours + ':' + (minutes < 10 ? `0${minutes}` : minutes) + ':' + (seconds < 10 ? `0${seconds}` : seconds);
+        }
+    },
+
     methods: {
 
         fetchApi(endpoint, success) {
@@ -36,7 +52,7 @@ export default {
 
     created() {
 
-        // Get all sections data and remap values
+        // Get playlist
         this.fetchApi(`/playlists/${this.$route.params.id}`, (data) => { this.media = data });
     }
 
@@ -49,31 +65,127 @@ export default {
     <AppLoader v-if="isLoading" />
 
     <!-- Page Content -->
-    <div v-else class="py-4">
+    <div v-else class="playlist-detail">
 
-        <!-- Page navigation -->
-        <nav class="py-2">
-            <ul>
-                <li>
-                    <BaseButton @click="$router.back()" icon="arrow-left" title="Back" size="xl" />
-                </li>
-            </ul>
-        </nav>
+        <!-- Content Top -->
+        <div class="playlist-top mb-3">
 
-        <!-- Content -->
-        <div class="row">
-            <div class="col">
-                <h2 class="mb-1">{{ media.title }}</h2>
-                <span class="col-gray-700 fs-5">{{ media.author }}</span>
+            <div class="row">
+
+                <div class="col-12 col-md-8 mb-3">
+
+                    <!-- Page navigation -->
+                    <nav class="mb-2">
+                        <ul>
+                            <li>
+                                <BaseButton @click="$router.back()" icon="arrow-left" title="Back" size="xl"
+                                    class="btn-light" />
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <!-- Media data -->
+                    <h2 class="mb-1">{{ media.title }}</h2>
+                    <span class="col-gray-700 fs-5">{{ media.author }}</span>
+
+                    <!-- Length Info -->
+                    <div class="playlist-length d-none d-md-flex">
+                        <h2 class="mb-0">{{ media.tracks.length }}</h2>
+                        <h6 class="mb-0">TRACKS</h6>
+                        <span>{{ totlaDuration }}</span>
+                    </div>
+
+                </div>
+
+                <!-- Cover -->
+                <div class="col-12 col-md-4">
+                    <img :src="media.cover" :alt="media.title" class="playlist-cover">
+                </div>
 
             </div>
-            <div class="col">
-                <img :src="media.cover" :alt="media.title">
-            </div>
-
         </div>
+
+        <!-- Tracks -->
+        <ul class="track-list p-3">
+            <li v-for="(track, idx) in media.tracks" :key="track.id">
+
+                <img :src="track.album.cover" :alt="track.title">
+
+                <span class="mx-3">{{ (idx + 1) }}</span>
+
+                <div>
+                    <h6 class="mb-0">{{ track.title }}</h6>
+                    <span>{{ track.album.author.name }}</span>
+                </div>
+            </li>
+        </ul>
+
     </div>
 </template>
 
 
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/vars' as *;
+
+
+.playlist-detail {
+
+    padding-bottom: 2rem;
+
+    .playlist-top {
+        padding: 1rem;
+        position: relative;
+
+        color: $col-light;
+        background-image: linear-gradient(130deg, $col-gray-900 20%, $col-dark);
+
+        .playlist-length {
+            padding: 0.5rem;
+            width: 100px;
+            height: 100px;
+            position: absolute;
+            left: 15px;
+            bottom: 15px;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            color: $col-dark;
+            background-color: $col-light;
+            border-radius: 50%;
+        }
+    }
+
+    .playlist-cover {
+        width: 100%;
+        height: 100%;
+
+        object-fit: cover;
+        box-shadow: 0 0 8px 4px rgba($color: #000, $alpha: 0.3);
+    }
+
+    .track-list {
+
+        li {
+            padding: 0.5rem 0;
+
+            display: flex;
+            align-items: center;
+            border-bottom: 1px solid $col-gray-300;
+
+            &:first-child {
+                border-top: 1px solid $col-gray-300;
+            }
+        }
+
+        img {
+            width: auto;
+            height: 40px;
+
+            object-fit: cover;
+        }
+    }
+
+}
+</style>
