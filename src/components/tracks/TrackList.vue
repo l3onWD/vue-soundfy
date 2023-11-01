812b0d2ac5e851921d39b109113525f715cf9c2e
l3on <l3onwebdev@gmail.com>
2023-11-01 19:35:15 +0100

    added track list component

---
+++ src/components/tracks/TrackList.vue
@@ -0,0 +1,78 @@
+<script>
+/* -----------------------------------------
+* RESOURCES
+-------------------------------------------*/
+/*** COMPONENTS ***/
+import BaseButton from '@/components/base/BaseButton.vue';
+
+
+export default {
+
+    components: { BaseButton },
+
+    props: {
+        tracks: {
+            type: Array,
+            default: []
+        }
+    }
+
+}
+</script>
+
+
+<template>
+    <h4>Tracks</h4>
+
+    <ul class="track-list">
+        <li v-for="(track, idx) in tracks" :key="track.id">
+
+            <div class="d-flex align-items-center flex-grow-1">
+
+                <img :src="track.album.cover" :alt="track.title">
+
+                <span class="mx-3">{{ (idx + 1) }}</span>
+
+                <div class="flex-grow-1">
+                    <h6 class="mb-0">{{ track.title }}</h6>
+                    <span>{{ track.album.author.name }}</span>
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <BaseButton icon="heart" iconStyle="far" title="Like" />
+                <BaseButton icon="list" title="Add to Next Up" />
+                <BaseButton icon="play" />
+            </div>
+        </li>
+    </ul>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../../assets/scss/vars' as *;
+
+
+.track-list {
+
+    li {
+        padding: 0.5rem 0;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid $col-gray-300;
+
+        &:first-child {
+            border-top: 1px solid $col-gray-300;
+        }
+    }
+
+    img {
+        width: auto;
+        height: 40px;
+
+        object-fit: cover;
+    }
+}
+</style>
--- src/pages/PlaylistDetailPage.vue
+++ src/pages/PlaylistDetailPage.vue
@@ -5,6 +5,7 @@
 /*** COMPONENTS ***/
 import AppLoader from '@/components/AppLoader.vue';
 import BaseButton from '@/components/base/BaseButton.vue';
+import TrackList from '@/components/tracks/TrackList.vue';
 
 /*** DATA ***/
 import axios from 'axios';
@@ -14,7 +15,7 @@ const baseUri = 'http://127.0.0.1:8000/api';
 
 export default {
 
-    components: { AppLoader, BaseButton },
+    components: { AppLoader, BaseButton, TrackList },
 
     data: () => ({
         playlist: null,
@@ -119,30 +120,8 @@ export default {
         <div class="playlist-bottom p-3">
 
             <!-- Tracks -->
-            <h4>Tracks</h4>
+            <TrackList :tracks="playlist.tracks" />
 
-            <ul class="track-list">
-                <li v-for="(track, idx) in playlist.tracks" :key="track.id">
-
-                    <div class="d-flex align-items-center flex-grow-1">
-
-                        <img :src="track.album.cover" :alt="track.title">
-
-                        <span class="mx-3">{{ (idx + 1) }}</span>
-
-                        <div class="flex-grow-1">
-                            <h6 class="mb-0">{{ track.title }}</h6>
-                            <span>{{ track.album.author.name }}</span>
-                        </div>
-                    </div>
-
-                    <div class="d-flex align-items-center">
-                        <BaseButton icon="heart" iconStyle="far" title="Like" />
-                        <BaseButton icon="list" title="Add to Next Up" />
-                        <BaseButton icon="play" />
-                    </div>
-                </li>
-            </ul>
         </div>
 
     </div>
@@ -184,29 +163,6 @@ export default {
 
         object-fit: cover;
         box-shadow: 0 0 8px 4px rgba($color: #000, $alpha: 0.3);
-    }
-
-    .track-list {
-
-        li {
-            padding: 0.5rem 0;
-
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            border-bottom: 1px solid $col-gray-300;
-
-            &:first-child {
-                border-top: 1px solid $col-gray-300;
-            }
-        }
-
-        img {
-            width: auto;
-            height: 40px;
-
-            object-fit: cover;
-        }
     }
 
 }
