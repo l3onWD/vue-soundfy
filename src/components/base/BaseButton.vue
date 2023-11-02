06399d6ebc0d0a8467bfca1b82e0104935c52b3a
l3on <l3onwebdev@gmail.com>
2023-11-02 19:27:42 +0100

    changed base button props

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,13 +1,16 @@
 <script>
 export default {
     props: {
-        label: String,
+
         icon: String,
+
+        iconSize: String,
+
         iconStyle: {
             type: String,
-            default: 'fas'
-        },
-        size: String
+            default: 'fas',
+            required: false
+        }
     }
 
 }
@@ -16,82 +19,7 @@ export default {
 
 <template>
     <button>
-        <FontAwesomeIcon v-if="icon" :icon="[iconStyle, icon]" :size="size" />
-        {{ label }}
+        <FontAwesomeIcon v-if="icon" :icon="[iconStyle, icon]" :size="iconSize" />
+        <slot></slot>
     </button>
 </template>
-
-
-<style lang="scss" scoped>
-@use '../../assets/scss/vars' as *;
-
-
-button {
-    padding: 0;
-    width: 2rem;
-    height: 2rem;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    line-height: 1;
-    color: $col-gray-900;
-
-    &:hover {
-        color: $col-dark;
-    }
-
-    &.active {
-        color: $col-orange;
-    }
-
-    &.btn-rounded {
-        border-radius: 50%;
-    }
-
-    &.btn-big {
-        width: 4rem;
-        height: 4rem;
-
-        font-size: 2.5rem;
-    }
-
-    &.btn-light {
-        color: $col-gray-700;
-
-        &:hover {
-            color: $col-light;
-        }
-    }
-
-    &.btn-disabled {
-        color: $col-gray-700;
-
-        &:hover {
-            color: $col-gray-700;
-        }
-    }
-
-    &.btn-orange {
-        color: $col-gray-300;
-        background-color: $col-orange;
-
-        &:hover {
-            color: $col-light;
-        }
-    }
-
-    &.btn-outline-orange {
-        color: $col-gray-700;
-        border: 1px solid $col-gray-700;
-
-        &:hover {
-            color: $col-light;
-            background-color: $col-orange;
-            ;
-        }
-    }
-}
-</style>
