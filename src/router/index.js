46972a996a70549b5231f8fcfd4add86bd08ab4f
l3on <l3onwebdev@gmail.com>
2023-11-02 12:57:27 +0100

    added detail page to all resource routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
-import PlaylistDetailPage from '@/pages/PlaylistDetailPage.vue';
+import MediaDetailPage from '@/pages/MediaDetailPage.vue';
 
 
 const router = createRouter({
@@ -8,10 +8,9 @@ const router = createRouter({
     linkExactActiveClass: 'active',
     routes: [
         { path: '/', name: 'home', component: HomePage },
-        { path: '/playlists/:id', name: 'playlist-detail', component: PlaylistDetailPage },
-        // !DEBUG
-        { path: '/albums/:id', name: 'album-detail', component: PlaylistDetailPage },
-        { path: '/tracks/:id', name: 'track-detail', component: PlaylistDetailPage },
+        { path: '/playlists/:id', name: 'playlist-detail', component: MediaDetailPage },
+        { path: '/albums/:id', name: 'album-detail', component: MediaDetailPage },
+        { path: '/tracks/:id', name: 'track-detail', component: MediaDetailPage },
 
         { path: '/:pathMatch(.*)*', redirect: '/' },
     ]
