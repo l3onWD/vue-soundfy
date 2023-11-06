f07b9fd5ec9e3a4cb8ecc75525c5f20a9e2cb817
l3on <l3onwebdev@gmail.com>
2023-11-06 20:31:23 +0100

    added base search emits and reset logic

--- src/components/base/BaseSearchInput.vue
+++ src/components/base/BaseSearchInput.vue
@@ -1,13 +1,27 @@
 <script setup>
 
+/*** DATA ***/
+defineProps(['modelValue'])
+defineEmits(['update:modelValue'])
+
 </script>
 
 
 <template>
     <div class="search-input">
+
+        <!-- Lens Icon -->
         <FontAwesomeIcon icon="fas fa-search" size="lg" class="search-input-lens" />
-        <input type="text" placeholder="Search...">
-        <FontAwesomeIcon icon="fas fa-close" size="lg" class="search-input-close" />
+
+        <!-- Input -->
+        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" type="text"
+            placeholder="Search...">
+
+        <!-- Reset Button -->
+        <button v-if="modelValue" @click="$emit('update:modelValue', '')" class="search-input-close">
+            <FontAwesomeIcon icon="fas fa-close" size="lg" />
+        </button>
+
     </div>
 </template>
 
@@ -16,25 +30,39 @@
 @use '@/assets/scss/vars' as *;
 
 .search-input {
+    height: 100%;
     position: relative;
 
     display: flex;
     align-items: center;
 
     input {
-        padding: 0.375rem 2rem;
+        padding: 0 2.5rem;
+        height: 100%;
+
+        border-radius: 50px;
     }
 
     &-lens,
     &-close {
-        padding: 0.5rem;
+        padding: 0.75rem;
         position: absolute;
         top: 50%;
         transform: translateY(-50%);
+
+        color: $col-gray-700;
+    }
+
+    &-lens {
+        left: 0;
     }
 
     &-close {
         right: 0;
+
+        &:hover {
+            color: $col-gray-900;
+        }
     }
 }
 </style>
