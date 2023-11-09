7b1d8926e5ced2b2b95d76214ddc344556d1b236
l3on <l3onwebdev@gmail.com>
2023-11-09 15:06:35 +0100

    added fetch api composable

---
+++ src/composables/useFetchApi.js
@@ -0,0 +1,32 @@
+import { ref, computed } from 'vue';
+import axios from 'axios';
+
+const baseUri = 'http://127.0.0.1:8000/api';
+const activeRequests = ref(0);// Global variable
+//TODO axios global settings
+
+export default function useFetchApi() {
+
+    // Loading state
+    const isLoading = computed(() => activeRequests.value);
+
+    // Get request
+    const makeGetRequest = async (endpoint, params) => {
+
+        // Update global state
+        activeRequests.value++;
+
+        // Make axios get request
+        try {
+            const { data } = await axios.get(baseUri + endpoint, { params });
+            return data;
+        } catch (err) {
+            console.error(err);
+        } finally {
+            // Update global state
+            activeRequests.value--;
+        }
+    }
+
+    return { isLoading, makeGetRequest };
+}
