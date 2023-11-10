6b667f48e499955f8eb87954fd0544da5978bc0c
l3on <l3onwebdev@gmail.com>
2023-11-10 13:27:18 +0100

    added suggestion list component

---
+++ src/components/filters/SearchSuggestionList.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+/*** COMPONENTS ***/
+import MediaDetailCard from '@/components/media/MediaDetailCard.vue';
+
+/*** PROPS ***/
+const props = defineProps({
+    suggestions: {
+        type: Array,
+        default: []
+    },
+    isLoading: Boolean,
+    searchTerm: String
+});
+
+</script>
+
+
+<template>
+    <ul class="suggestions">
+
+        <li v-for="suggestion in props.suggestions" :key="suggestion.id">
+            <MediaDetailCard :media="suggestion" small />
+        </li>
+
+        <li>
+            <RouterLink :to="{ name: 'search', query: { title: props.searchTerm } }">
+                <FontAwesomeIcon icon="fas fa-search" />
+                Search for "{{ props.searchTerm }}"
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+.suggestions {
+    color: $col-light;
+    background-color: $col-dark;
+
+    li {
+        padding: 0.5rem;
+
+        &:hover {
+            background-color: $col-gray-900;
+        }
+    }
+
+}
+</style>
