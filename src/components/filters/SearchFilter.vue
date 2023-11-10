307ca288f375eeac7c73c9bc888f9f2dc6626f41
l3on <l3onwebdev@gmail.com>
2023-11-10 13:37:30 +0100

    added suggestion list logic

--- src/components/filters/SearchFilter.vue
+++ src/components/filters/SearchFilter.vue
@@ -1,19 +1,21 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
+import { ref, reactive, watchEffect } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import useFetchApi from '@/composables/useFetchApi';
 
 
 /*** COMPONENTS ***/
 import BaseSearchInput from '@/components/base/BaseSearchInput.vue';
+import SearchSuggestionList from '@/components/filters/SearchSuggestionList.vue';
 
 
 /*** DATA ***/
 const router = useRouter();
 const route = useRoute();
-const { makeGetRequest } = useFetchApi();
+const { isLoading, makeGetRequest } = useFetchApi();
 const searchTerm = ref('');
 let searchThrottleId = null;
+const suggestions = ref([]);
 
 
 /*** LOGIC ***/
@@ -31,8 +33,14 @@ const suggestSearch = () => {
     clearTimeout(searchThrottleId);
     searchThrottleId = setTimeout(async () => {
 
-        const suggestions = await makeGetRequest('/search', { title: searchTerm.value });
-        console.log(suggestions);
+        const results = await makeGetRequest('/search', { title: searchTerm.value });
+
+        console.log(results);
+        if (!results.length) return;
+
+        suggestions.value.splice(0);
+        suggestions.value.push(...results);
+        suggestions.value.forEach(media => { if (media.kind === 'track') setMediaTrack(media, 'track') });
 
     }, 500);
 };
@@ -47,11 +55,39 @@ const handleSearchSubmit = () => {
     router.push({ name: 'search', query: { title: searchTerm.value } })
 }
 
+
+// Set Media track
+const setMediaTrack = (media) => {
+
+    return {
+        ...media,
+        tracks: [{ ...media, uid: `track-${media.id}` }]
+    }
+}
+
 </script>
 
 
 <template>
-    <form action="" @submit.prevent="handleSearchSubmit">
+    <form class="search-filter" @submit.prevent="handleSearchSubmit">
+
         <BaseSearchInput @input="suggestSearch" v-model.trim="searchTerm" />
+
+        <div class="search-filter-menu">
+            <SearchSuggestionList :suggestions="suggestions" :isLoading="isLoading" :searchTerm="searchTerm" />
+        </div>
     </form>
 </template>
+
+<style lang="scss" scoped>
+.search-filter {
+    position: relative;
+
+    &-menu {
+        position: absolute;
+        top: calc(100% + 8px);
+        left: 0;
+        right: 0;
+    }
+}
+</style>
