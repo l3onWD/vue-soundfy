d70e7919dcca57957eee6dafb9375f904a19a791
l3on <l3onwebdev@gmail.com>
2023-11-13 16:45:20 +0100

    refactored search page

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { ref, reactive, watch, computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { reactive, watch, computed } from 'vue';
 import useFetchApi from '@/composables/useFetchApi';
 
 /*** COMPONENTS ***/
@@ -8,51 +7,47 @@ import AppLoader from '@/components/AppLoader.vue';
 import MediaSearchCard from '@/components/media/MediaSearchCard.vue';
 
 
+/*** ROUTER PROPS ***/
+const { searchedTerm } = defineProps(['searchedTerm']);
+
 /*** DATA ***/
-const route = useRoute();
-const { makeGetRequest } = useFetchApi();
-const isLoading = ref(false);
+const { isLoading, makeGetRequest } = useFetchApi();
 const results = reactive([]);
 
-
 /*** COMPUTED ***/
-const searchedPlaylists = computed(() => results.filter(({ kind }) => kind === 'playlist'));
-const searchedAlbums = computed(() => results.filter(({ kind }) => kind === 'album'));
-const searchedTracks = computed(() => results.filter(({ kind }) => kind === 'track'));
+const playlists = computed(() => results.filter(({ kind }) => kind === 'playlist'));
+const albums = computed(() => results.filter(({ kind }) => kind === 'album'));
+const tracks = computed(() => results.filter(({ kind }) => kind === 'track'));
 
 
 /*** LOGIC ***/
-// Set Media track
-const setMediaTrack = (media) => {
-
-    return {
-        ...media,
-        tracks: [{ ...media, uid: `track-${media.id}` }]
-    }
-}
-
 // Listen to query change
-watch(() => route.query.title, async () => {
+watch(() => searchedTerm, () => {
 
-    isLoading.value = true;
+    // Reset Results
     results.splice(0);
 
-    // Get searched results
-    await makeGetRequest('/search', { title: route.query.title })
+    // Search API
+    makeGetRequest('/search', { title: searchedTerm })
         .then(data => {
 
+            // Check if there aren't results
             if (!data.length) return;
+
             results.push(...data);
 
-            // Fix track media
-            results.forEach(media => { if (media.kind === 'track') setMediaTrack(media, 'track') });
+            // Add more details if is a track media [TODO da eliminare]
+            results.forEach(media => {
+                if (media.kind === 'track') {
+                    media = {
+                        ...media,
+                        tracks: [{ ...media, uid: `track-${media.id}` }]
+                    }
+                }
+            });
         });
 
-    isLoading.value = false;
-
 }, { immediate: true });
-
-
 
 </script>
 
@@ -72,37 +67,37 @@ watch(() => route.query.title, async () => {
             <div v-if="results.length">
 
                 <!-- Playlists -->
-                <div v-if="searchedPlaylists.length" class="mb-3">
+                <div v-if="playlists.length" class="mb-3">
 
                     <h5>Playlists:</h5>
                     <hr>
 
-                    <div v-for="media in searchedPlaylists" :key="media.id" class="mb-3">
-                        <MediaSearchCard :media="media" />
+                    <div v-for="playlist in playlists" :key="playlist.id" class="mb-3">
+                        <MediaSearchCard :media="playlist" />
                     </div>
 
                 </div>
 
                 <!-- Albums -->
-                <div v-if="searchedAlbums.length" class="mb-3">
+                <div v-if="albums.length" class="mb-3">
 
                     <h5>Albums:</h5>
                     <hr>
 
-                    <div v-for="media in searchedAlbums" :key="media.id" class="mb-3">
-                        <MediaSearchCard :media="media" />
+                    <div v-for="album in albums" :key="album.id" class="mb-3">
+                        <MediaSearchCard :media="album" />
                     </div>
 
                 </div>
 
                 <!-- Tracks -->
-                <div v-if="searchedTracks.length" class="mb-3">
+                <div v-if="tracks.length" class="mb-3">
 
                     <h5>Tracks:</h5>
                     <hr>
 
-                    <div v-for="media in searchedTracks" :key="media.id" class="mb-3">
-                        <MediaSearchCard :media="media" />
+                    <div v-for="track in tracks" :key="track.id" class="mb-3">
+                        <MediaSearchCard :media="track" />
                     </div>
 
                 </div>
