9a506a068cd9eae27dd1265eb15d6f5e11f17820
l3on <l3onwebdev@gmail.com>
2023-11-15 15:43:22 +0100

    added error throwing on fetch composable

--- src/composables/useFetchApi.js
+++ src/composables/useFetchApi.js
@@ -18,16 +18,30 @@ export default function useFetchApi(loader = true) {
     // Get request
     const makeGetRequest = async (endpoint, params) => {
 
-        // Update global state
-        if (loader) activeRequests.value++;
+        try {
+            // Update global loading state
+            if (loader) activeRequests.value++;
 
-        // Make axios get request
-        const response = await apiClient.get(endpoint, { params });
+            // Make axios get request
+            const response = await apiClient.get(endpoint, { params });
 
-        // Update global state
-        if (loader) activeRequests.value--;
+            // Remove this request from loading state
+            if (loader) activeRequests.value--;
 
-        return response;
+            return response;
+
+        } catch (err) {
+
+            // Remove this request from loading state
+            if (loader) activeRequests.value--;
+
+            // Check if is a network error [0] or there is a status code
+            const status = err.response ? err.response.status : 0;
+
+            throw status;
+
+        }
+
     }
 
     return { isLoading, makeGetRequest };
