d4e53c7780f163b1d9af3925e0f85db646a7dca1
l3on <l3onwebdev@gmail.com>
2023-11-15 19:44:07 +0100

    added message container and imported in app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useFlashMessagesStore } from '@/stores/FlashMessagesStore';
 
 /*** COMPONENTS ***/
+import AppMessages from '@/components/AppMessages.vue';
 import AppMain from '@/components/AppMain.vue';
 import AppBottom from '@/components/AppBottom.vue';
+
+
+/*** DATA ***/
+const messagesStore = useFlashMessagesStore();
+const { messages } = storeToRefs(messagesStore);
 
 </script>
 
 
 <template>
     <div class="d-flex flex-column vh-100">
+
+        <!-- Flash Messages Container -->
+        <AppMessages :messages="messages" />
+
+        <!-- Main Content -->
         <AppMain />
+
+        <!-- Bottom Content -->
         <AppBottom />
     </div>
 </template>
---
+++ src/components/AppMessages.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+/*** COMPONENTS ***/
+import BaseFlashMessage from '@/components/base/BaseFlashMessage.vue';
+
+
+/*** PROPS ***/
+defineProps({
+    messages: {
+        type: Array,
+        default: []
+    }
+});
+
+</script>
+
+
+<template>
+    <TransitionGroup name="app-messages" tag="ul" class="app-messages">
+        <li v-for="message in messages" :key="message.id">
+            <BaseFlashMessage :message="message" class="mb-2" />
+        </li>
+    </TransitionGroup>
+</template>
+
+
+<style lang="scss" scoped>
+.app-messages {
+    position: fixed;
+    bottom: 110px;
+    right: 20px;
+    width: 250px;
+
+    z-index: 2;
+
+    // Animations
+    &-enter-active,
+    &-leave-active {
+        transition: all 0.5s ease;
+    }
+
+    &-enter-from,
+    &-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
+    }
+}
+
+// MEDIA MD
+@media screen and (min-width: 768px) {
+    .app-messages {
+        bottom: 60px;
+    }
+}
+</style>
